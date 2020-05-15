34b43e9223b46f95c496fb2ef53ef16286b6f987
Anastasiia Tychkova <tychkovaam@gmail.com>
2020-05-15 15:32:26 -0700

    refactored the Stories block, transfered the grid layout to a separate component

--- pages/stories/index.vue
+++ pages/stories/index.vue
@@ -1,26 +1,40 @@
 <template>
-  <div>
-    <stories>
-      <slot>
-        <input-search>
-          <button-search>Поиск</button-search>
-        </input-search>
-      </slot>
-    </stories>
+  <div class="container">
+    <h2 class="stories__title">Истории неизлечимых привычек</h2>
+    <slot>
+      <input-search>
+        <button-search>Поиск</button-search>
+      </input-search>
+    </slot>
+    <stories-grid />
   </div>
 </template>
 
 <script>
-import Stories from '@/components/Stories';
+import StoriesGrid from '@/components/StoriesGrid';
 import Button from '@/components/ui/Button';
 import Input from '@/components/ui/Input';
 export default {
   components: {
-    stories: Stories,
+    'stories-grid': StoriesGrid,
     'button-search': Button,
     'input-search': Input,
   },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.container {
+  max-width: 1320px;
+  margin: 0 auto;
+  padding-bottom: 100px;
+  padding-top: 100px;
+}
+.stories__title {
+  font-weight: 600;
+  font-size: 32px;
+  line-height: 36px;
+  max-width: 413px;
+  padding-bottom: 70px;
+}
+</style>
